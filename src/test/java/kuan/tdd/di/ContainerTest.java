13a6a1b41adcef20475cd4b6f1c77c6c63555d26
秦学宽 <qinxuekuan@qianxin.com>
2022-06-02 18:53:09 +0800

    1th: 搭好框架，列出部分任务分解

---
+++ src/test/java/kuan/tdd/di/ContainerTest.java
@@ -0,0 +1,50 @@
+package kuan.tdd.di;
+
+import org.junit.jupiter.api.Nested;
+
+/**
+ * @author qinxuekuan
+ * @date 2022/6/2
+ */
+public class ContainerTest {
+
+    @Nested
+    public class ComponentConstruction {
+        // TODO instance
+        // TODO abstract class
+        // TODO interface
+
+
+        @Nested
+        public class ConstructorInjection{
+            // TODO no args constructor
+            // TODO with dependencies
+            // TODO A --> B --> C
+
+        }
+
+        @Nested
+        public class FieldInjection{
+
+        }
+
+        @Nested
+        public class MethodInjection{
+
+        }
+
+    }
+
+
+    @Nested
+    public class DependenciesSelection {
+
+    }
+
+    @Nested
+    public class LifecycleManagement {
+
+    }
+
+
+}
