ce34ffafef5cfb17cbad861a2adeb627b1568cf3
秦学宽 <qinxuekuan@qianxin.com>
2022-07-04 13:27:27 +0800

    13th。 添加并实现 happy path 测试： 对 Provider<T> 这样的依赖，不应该有【循环依赖问题】的存在。

--- src/test/java/kuan/tdd/di/ContextTest.java
+++ src/test/java/kuan/tdd/di/ContextTest.java
@@ -376,5 +376,22 @@ class ContextTest {
         }
 
 
+        static class CyclicDependencyProviderConstructor implements Dependency {
+            @Inject
+            public CyclicDependencyProviderConstructor(Provider<Component> component) {
+
+            }
+        }
+
+        @Test
+        public void should_not_throw_exception_if_cyclic_dependency_via_provider() {
+            config.bind(Component.class, CyclicComponentInjectConstructor.class);
+            config.bind(Dependency.class, CyclicDependencyProviderConstructor.class);
+
+            Context context = config.getContext();
+            assertTrue(context.get(Component.class).isPresent());
+        }
+
+
     }
 }
