1b9d5271352090ab7ab5343cbc6296f0c0b5c3e5
秦学宽 <qinxuekuan@qianxin.com>
2022-07-16 14:32:44 +0800

    19th。 添加 should_throw_exception_if_multi_qualifiers_given 测试。

--- src/test/java/kuan/tdd/di/InjectionTest.java
+++ src/test/java/kuan/tdd/di/InjectionTest.java
@@ -183,8 +183,19 @@ class InjectionTest {
                         provider.getDependencies().toArray());
             }
 
+            static class MultiQualifierInjectConstructor {
+                @Inject
+                public MultiQualifierInjectConstructor(@Named("ChosenOne") @Skywalker Dependency dependency) {
 
-            // todo throw illegal component if illegal qualifier given to injection point
+                }
+            }
+
+            @Test
+            public void should_throw_exception_if_multi_qualifiers_given() {
+                assertThrows(IllegalComponentException.class,
+                        () -> new InjectionProvider<>(MultiQualifierInjectConstructor.class));
+
+            }
         }
     }
 
